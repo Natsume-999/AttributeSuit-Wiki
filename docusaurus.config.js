b9d3c68707b96e9ba2a23b23c23915c9b9835817
Natsume-999 <148447869+Natsume-999@users.noreply.github.com>
2023-10-19 20:13:01 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '属性套装',
+  title: 'AttributeSuit',
   tagline: '基于 AttributePlus 3x 套装',
   favicon: 'img/favicon.ico',
 
