1e83255b00edcbbc671570b7ff13e6ef0dfb5d1d
Natsume-999 <148447869+Natsume-999@users.noreply.github.com>
2024-03-16 22:07:34 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://Natsume-999.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/GreyFantasy-Wiki/',
+  baseUrl: '/AttributeSuit-Wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
